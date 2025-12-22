50046e38381452149a42be0cf1a92d4ebcd693ca
marcocentofanti <marcocentofanti@hotmail.it>
2025-12-22 18:19:56 +0100

    Start and first snack

---
+++ 01_saluti/script.js
@@ -0,0 +1,17 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Tizio';
+
+
+// Dichiara la funzione qui
+
+function saluta(nome){ 
+  return `Ciao ${nome}`
+}
+
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(saluta(userName));
+
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
