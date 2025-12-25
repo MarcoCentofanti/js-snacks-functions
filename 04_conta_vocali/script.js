10e6d9ccfea038591e1311c2eb8443760a90e71a
MarcoCentofanti <116574494+MarcoCentofanti@users.noreply.github.com>
2025-12-25 11:44:29 +0100

    fourth snack completed

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,25 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
-
+function contaVocali(stringa) {
+  let numeroVocali = 0;
+  let stringaMinuscola = stringa.toLowerCase();
+  for (i = 0; i < stringa.length; i++) {
+    if (
+      stringa[i] === "a" ||
+      stringa[i] === "e" ||
+      stringa[i] === "i" ||
+      stringa[i] === "o" ||
+      stringa[i] === "u"
+    ) {
+      numeroVocali++;
+    }
+  }
+  return numeroVocali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(contaVocali(word));
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
